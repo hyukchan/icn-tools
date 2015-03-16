d47af9ef4165ae4666cde85af3c5a6af84e50aaf
David Gautier <david.gautier@ginko.io>
2015-03-16 08:44:41 +0100

    added new request parameter type "server"

--- src/main/java/com/github/dgautier/icn/RequestParameters.java
+++ src/main/java/com/github/dgautier/icn/RequestParameters.java
@@ -7,6 +7,7 @@ public class RequestParameters {
 
     public static final String REPOSITORY_ID = "repositoryId";
     public static final String REPOSITORY_TYPE = "repositoryType";
+    public static final String SERVER = "server";
     public static final String SERVER_TYPE = "serverType";
     public static final String TEMPLATE_NAME = "template_name";
     public static final String FILTER_TYPE = "filter_type";
